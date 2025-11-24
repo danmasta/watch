e16c224c379e8e70ffe2f6d531eed66cbba21d56
Daniel Smith <d@danmasta.net>
2025-11-23 18:32:16 -0800

    add main entrypoint

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Watcher, watcher, watch } from './lib/watcher.js';
+import { Watchmon, watchmon } from './lib/watchmon.js';
+
+export {
+    Watcher,
+    watcher,
+    watch,
+    Watchmon,
+    watchmon
+};
+
+export default {
+    Watcher,
+    watcher,
+    watch,
+    Watchmon,
+    watchmon
+};
